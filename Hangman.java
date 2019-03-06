b685c566d099523d29a8c61c779f3a7cbbe5cb64
Albert Alises <albert.alises@gmail.com>
2019-03-06 15:18:25 +0100

    Readability changes, validate word, check if letter on input is valid, shortfuse game if word does not match.

--- Hangman.java
+++ Hangman.java
@@ -5,48 +5,49 @@ import java.util.regex.Pattern;
 public class Hangman {
 
 	public static void main(String[] args) {
-		final int MAX_ERRORS = 10;	
-		final String ERROR_FORMAT = "Format error: You must write one word without numbers nor special characters!!";
+		final int MAX_ERRORS = 10, NUM_SPACES = 25;	
+		final String FORMAT_ERROR = "Format error: No numbers or special characters allowed";
+
 		Scanner in;
 		boolean loser = false, winner = false;
-		in = new Scanner(System.in);
-		
+			
 		System.out.println("Write a word: ");
-		String wordString = in.next();
-		wordString = wordString.toLowerCase();
-		
-		Pattern pattern = Pattern.compile("[a-z]*");
-		Matcher matcher = pattern.matcher(wordString);
-	 
-		if (!matcher.matches()) {
-			System.out.println(ERROR_FORMAT);
-			System.out.println("Game finished!!");
-		}else {
-			char[] word = wordString.toCharArray();
-			int wordSize = word.length;
-			
-			char letter;
-			int errors = 0;
-		
-			for(int i = 0;i<25;i++) System.out.println("");
-			
-			char[] encryptedWord = Utils.createEncryptedWord(wordSize);		
-		    while(!loser && !winner) {
-		    	System.out.println("Enter a letter: ");
-		    	letter = Character.toLowerCase(in.next().charAt(0));
-		    	
-		    	if(!Utils.revealAsterisks(encryptedWord,word,letter)) errors +=1;	
-		    	
-		    	System.out.println(new String(encryptedWord) +" | Errors: "+errors);
-		    	
-		    	if(errors == MAX_ERRORS) loser = true;
-		    	if(Utils.isRevealed(encryptedWord)) winner = true;
-		    }
-		    
-		    if(winner) System.out.println("You win!!");
-		    else System.out.println("Game over!!");
-		    
-		    in.close();
+
+		in = new Scanner(System.in);
+		String wordString = in.next().toLowerCase();
+			 
+		//Finish the game due to an error format if the input word does not match
+		if (!Utils.validateWord(wordString)) {
+			System.out.println(String.format("%s, Game finished!!", FORMAT_ERROR));
+			in.close();
+			return;
 		}
+
+		char[] word = wordString.toCharArray();
+		int wordSize = word.length;
+			
+		char letter;
+		int errors = 0;
+		
+		Utils.printSpaces(NUM_SPACES); //Print some padding spaces
+		char[] encryptedWord = Utils.createEncryptedWord(wordSize);	
+	
+		while(!(loser || winner)) {
+		  System.out.println("Enter a letter: ");
+			letter = Character.toLowerCase(in.next().charAt(0));
+			
+			if(!Utils.validateWord(Character.toString(letter))){
+				System.out.println(FORMAT_ERROR);
+				continue;
+			} 
+
+			if(!Utils.revealAsterisks(encryptedWord,word,letter)) errors +=1;	
+		  System.out.println(new String(encryptedWord) +" | Errors: " + errors);
+			
+			if(errors == MAX_ERRORS) loser = true;
+		  if(Utils.isRevealed(encryptedWord)) winner = true;
+		}
+		System.out.println(winner ? "You win!!" : "Game over!!");
+		in.close();
 	}
 }
